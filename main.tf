cdfe290d052a21933b6aeedba0ecc7fa657699ae
Paolo Mainardi <paolo.mainardi@sparkfabrik.com>
2022-01-28 12:42:44 +0100

    add a versions.tf file with required providers

--- main.tf
+++ main.tf
@@ -1,14 +1,3 @@
-terraform {
-  required_version = ">= 1.0"
-
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 3.0"
-    }
-  }
-}
-
 provider "google" {
   project = var.gcp_project
   region = var.gcp_region
